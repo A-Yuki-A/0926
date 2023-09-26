34e11fa971fcb15cf89d5039b675ffc3369b7764
A-Yuki-A <146058288+A-Yuki-A@users.noreply.github.com>
2023-09-26 06:33:27 +0000

    修正

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,4 @@ if st.button('乱数を生成'):
     # 最小値と最大値の間で乱数を生成
 for i in range(5):
     random_num = random.randint(min_val, max_val)
-    st.write(f'生成された乱数: {random_num}')
+st.write(f'生成された乱数: {random_num}')
